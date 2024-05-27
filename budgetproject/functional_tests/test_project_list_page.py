ca284ab99a1bde9d30bfe3ff5f14b357714bb06c
gamersumit <saggarwal@innow8apps.com>
2024-05-27 16:02:00 +0530

    functional testing pending

---
+++ budgetproject/functional_tests/test_project_list_page.py
@@ -0,0 +1,19 @@
+# from selenium import webdriver
+# from budget.models import Project
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase 
+# import time
+
+# class TestProjectListPage(StaticLiveServerTestCase):
+#     def test_foo(self):
+#         self.assertEquals(0, 1)
+    
+#     def setup(self):
+#         self.browser = webdriver.chrome('functional_tests/chromedriver.exe')
+    
+
+#     def tearDown(self):
+#         self.browser.close()
+
+#     def test_no_projects_alert_is_displayed(self):
+#         self.browser.get(self.live_server_url)
+#         time.sleep(20)
