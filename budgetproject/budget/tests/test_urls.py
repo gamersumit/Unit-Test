b975bc3c06af54141e6b2a671f5fdaec8c80bcd9
gamersumit <saggarwal@innow8apps.com>
2024-05-27 14:52:28 +0530

    views and url unit testing

---
+++ budgetproject/budget/tests/test_urls.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from budget.views import project_list, project_detail, ProjectCreateView
+
+
+class TestUrls(SimpleTestCase):
+    def test_list_url_is_resolved(self):
+        url = reverse('list')
+        print("url -------> ", url)
+        print("resolve url -------> ", resolve(url))
+        self.assertEquals(resolve(url).func, project_list)
+
+    def test_detail_url_is_resolved(self):
+        url = reverse('detail', args=['some-slug'])
+        self.assertEquals(resolve(url).func, project_detail)
+
+    def test_add_url_is_resolved(self):
+        url = reverse('add')
+        self.assertEquals(resolve(url).func.view_class, ProjectCreateView)
+    
